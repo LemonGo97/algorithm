411e9fdd427c3f2202fc58218e73bd98f6c381ae
LemonGo97 <lemongo97@outlook.com>
2021-09-13 23:28:55 +0800

    双向链表删除节点架子

---
+++ src/node/RemoveDoubleLinkedNode.java
@@ -0,0 +1,18 @@
+package node;
+
+/**
+ * @author LemonGo97
+ */
+public class RemoveDoubleLinkedNode {
+    public static void main(String[] args) {
+
+    }
+
+    private static DoubleNode<String> remove(DoubleNode<String> head){
+
+
+
+
+        return null;
+    }
+}
